091d77cd3fc9995bd423951106c79465afb8a61b
projectmaj123-art <projectmaj123@gmail.com>
2026-09-02 23:33:07 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Scissors, Sparkles } from 'lucide-react'
-import { videoclipper } from '@/components/video-clipper'
+import { VideoClipper } from '@/components/video-clipper'
 
 export default function Page() {
   return (
@@ -31,7 +31,7 @@ export default function Page() {
           </p>
         </section>
 
-        <videoclipper/>
+        <VideoClipper/>
       </div>
     </main>
   )
